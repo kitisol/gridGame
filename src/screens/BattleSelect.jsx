a0e3a4d6957a3e523f1d5bd65ba19d80ae108769
kitisol <tyrvek@yahoo.com>
2021-09-19 23:00:53 -0400

    Updated display logic for battle list
    * Made the completion icon a new component
    * Added completion logic to change icon display
    * Generate battle list from an array of unlocked levels

--- src/screens/BattleSelect.jsx
+++ src/screens/BattleSelect.jsx
@@ -1,27 +1,10 @@
 import React, { createRef, useEffect, useState } from 'react';
 import '../styles/screenStyles.css';
 
-export default function BattleSelect() {
-  const svgRef = createRef();
-  const [dimensions, setDimensions] = useState({ width: 1, hieght: 1 });
-  const [windowSize, setWindowSize] = useState(window.innerWidth);
-
-  React.useEffect(() => {
-    function handleResize() {
-      setWindowSize(window.innerWidth);
-    }
-    window.addEventListener('resize', handleResize);
-  });
-
-  React.useEffect(() => {
-    if (svgRef.current) {
-      const { current } = svgRef;
-      const boundingRect = current.getBoundingClientRect();
-      const { width, height } = boundingRect;
-      setDimensions({ width: Math.round(width), height: Math.round(height) });
-    }
-  }, [svgRef.current, windowSize]);
-  let svg = (
+export function CompletionIcon(props) {
+  console.log(props);
+  let { stars } = props;
+  return (
     <svg viewBox="0 0 160.3 160.3">
       <g className="ring">
         <path
@@ -40,18 +23,18 @@ export default function BattleSelect() {
 			c-6.7,24.5-26.1,43.8-50.7,50.5v8.2c35.1-8.5,61.2-40.2,61.2-77.9S134.2,10.8,99.1,2.3z"
         />
       </g>
-      <g className="top-tine complete">
+      <g className={stars > 0 ? 'complete' : 'incomplete'}>
         <polygon class="outer" points="96.8,68.9 81,82 65.2,68.9 81,26.2 " />
         <polygon class="inner" points="93.5,68.2 81,78.5 68.6,68.2 81,34.5 " />
       </g>
-      <g className="left-tine complete">
+      <g className={stars > 1 ? 'complete' : 'incomplete'}>
         <polygon class="outer" points="63.7,72 79.4,85.1 79.4,95.7 32.7,114 " />
         <polygon
           class="inner"
           points="63.8,76.2 76.2,86.5 76.3,94.9 39.4,109.4 "
         />
       </g>
-      <g className="right-tine incomplete">
+      <g className={stars > 2 ? 'complete' : 'incomplete'}>
         <polygon
           class="outer"
           points="98.2,72 82.5,85.1 82.4,95.7 129.5,114 "
@@ -63,6 +46,95 @@ export default function BattleSelect() {
       </g>
     </svg>
   );
+}
+
+export default function BattleSelect() {
+  const svgRef = createRef();
+  const [dimensions, setDimensions] = useState({ width: 1, hieght: 1 });
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+  const levels = [
+    {
+      title: 'Basic Simulation',
+      details: 'Level 1 – Movement',
+      stars: 3,
+    },
+    {
+      title: 'Basic Simulation',
+      details: 'Level 2 – Obstacles',
+      stars: 3,
+    },
+    {
+      title: 'Basic Simulation',
+      details: 'Level 3 – Area Affects',
+      stars: 2,
+    },
+    {
+      title: 'Basic Simulation',
+      details: 'Level 4 – Space Station',
+      stars: 1,
+    },
+    {
+      title: 'Basic Simulation',
+      details: 'Level 5 – Movement',
+      stars: 1,
+    },
+    {
+      title: 'Basic Simulation',
+      details: 'Level 6 – Melee I',
+      stars: 2,
+    },
+    {
+      title: 'Basic Simulation',
+      details: 'Level 7 – Melee II',
+      stars: 1,
+    },
+    {
+      title: 'Basic Simulation',
+      details: 'Level 8 – Ranged I',
+      stars: 1,
+    },
+    {
+      title: 'Basic Simulation',
+      details: 'Level 9 – Ranged II',
+      stars: 1,
+    },
+    {
+      title: 'Basic Simulation',
+      details: 'Level 10 – Uniform & Tango',
+      stars: 0,
+    },
+  ];
+  React.useEffect(() => {
+    function handleResize() {
+      setWindowSize(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize);
+  });
+
+  React.useEffect(() => {
+    if (svgRef.current) {
+      const { current } = svgRef;
+      const boundingRect = current.getBoundingClientRect();
+      const { width, height } = boundingRect;
+      setDimensions({ width: Math.round(width), height: Math.round(height) });
+    }
+  }, [svgRef.current, windowSize]);
+
+  const battleList = levels.map((battle) => {
+    return (
+      <div className="d-flex battle-listing">
+        <div className="battle-icon">
+          <CompletionIcon stars={battle.stars} />
+        </div>
+        <div className="d-flex vertical-center">
+          <div className="battle-description">
+            <div className="battle-title">{battle.title}</div>
+            <div className="battle-details">{battle.details}</div>
+          </div>
+        </div>
+      </div>
+    );
+  });
   return (
     <div className="background">
       <div className="container">
@@ -73,15 +145,14 @@ export default function BattleSelect() {
             points="2,0 65,0 65,4 96,4 96,55 100,55 100,200, 45,200 45,196 2,196"
           />
         </svg>
-        <div
-          className="content"
-          style={{ height: `${dimensions.height - 50}px` }}
-        >
+        <div className="content">
           <div className="title">TRAINING</div>
           <div className="subTitle">SIMULATIONS</div>
-          <div className="d-flex">
-            <div className="">{svg}</div>
-            <div className="test">MELEE</div>
+          <div
+            className="battle-list"
+            style={{ height: `${dimensions.height - 150}px` }}
+          >
+            {battleList}
           </div>
         </div>
       </div>
